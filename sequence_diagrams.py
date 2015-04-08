54ba3bed01d93cf1ccb9cafcb41544e25fda02ca
Mike Gaffney <mr.gaffo@gmail.com>
2015-04-08 10:33:55 -0700

    Coded, needs test

--- sequence_diagrams.py
+++ sequence_diagrams.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os, urllib.parse, urllib.request
 
 class WebSequenceDiagramPlugin(sublime_plugin.EventListener):
 	def __init__(self):
@@ -17,4 +17,44 @@ class WebSequenceDiagramPlugin(sublime_plugin.EventListener):
 			return
 
 		print("saving sequence")
-		print(view.settings().get("sequence_server"))
+
+		filename = None
+		contents = ""
+		with open(view.file_name(), "r") as ins:
+			for line in ins:
+				if line.startswith(":"):
+					if filename:
+						self.fetch_diagram(
+							view.settings().get("sequence_server"),
+							contents,
+							filename)
+					filename = os.path.join(os.path.dirname(view.file_name()), line[1:].strip() + ".png")
+					contents = ""
+					continue
+				contents += line
+
+
+
+	def fetch_diagram(self, server, text, outputFile, style = 'vs2010' ):
+		print(server)
+		request = {}
+		request["message"] = text
+		request["style"] = style
+		request["apiVersion"] = "1"
+
+		url = urllib.parse.urlencode(request)
+
+		f = urllib.request.urlopen("http://" + server + "/", str.encode(url))
+		line = f.readline()
+		f.close()
+
+		expr = re.compile("(\?(img|pdf|svg)=[a-zA-Z0-9]+)")
+		m = expr.search(line)
+
+		if m == None:
+			print("Invalid response from server.")
+			return False
+
+		urllib.request.urlretrieve("http://" + server + "/" + m.group(0),
+				outputFile )
+		return True
